4f341514349870aaf8fbf6c98df5f9cc93aaa05c
Mohamed Hosny <mamedhosny345@gmail.com>
2024-08-24 20:55:03 +0300

    sec commit

--- src/Components/Registear/Registear.jsx
+++ src/Components/Registear/Registear.jsx
@@ -105,7 +105,7 @@ export default function Registear() {
             name="email"
             id="email"
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=""
+            placeholder=" "
           />
           <label
             htmlFor="email"
@@ -126,7 +126,7 @@ export default function Registear() {
             name="phone"
             id="phone"
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=""
+            placeholder=" "
           />
           <label
             htmlFor="phone"
@@ -147,7 +147,7 @@ export default function Registear() {
             name="password"
             id="password"
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=""
+            placeholder=" "
           />
           <label
             htmlFor="password"
@@ -168,7 +168,7 @@ export default function Registear() {
             name="rePassword"
             id="rePassword"
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=""
+            placeholder=" "
           />
           <label
             htmlFor="rePassword"
